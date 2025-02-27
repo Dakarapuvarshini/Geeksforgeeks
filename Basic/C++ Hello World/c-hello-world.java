37156622340bebe154c1b2d628ca2d7a4afda013
varshini dakarapu <Dakarapuvarshini>
2025-02-27 19:22:27 +0530

    first commit

---
+++ Basic/C++ Hello World/c-hello-world.java
@@ -0,0 +1,18 @@
+// Driver Code Starts
+class Solution {
+    public void helloWorld() {
+        // Type below System.out.println("Hello World");
+        // System.out will output to console
+        System.out.println("Hello World");
+    }
+}
+
+// Driver Code Starts
+public class Main {
+    public static void main(String[] args) {
+        Solution ob = new Solution();
+        // function call
+        ob.helloWorld();
+        System.out.println("~");
+    }
+}
