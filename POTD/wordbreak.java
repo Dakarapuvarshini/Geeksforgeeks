f1ae0618d375de89534fe0eae0dbd09c75f02cad
Dakarapuvarshini <116934987+Dakarapuvarshini@users.noreply.github.com>
2025-03-26 22:49:42 +0530

    updated

---
+++ POTD/wordbreak.java
@@ -0,0 +1,24 @@
+static boolean wordBreakRec(int i, String s,
+                                String[] dictionary)
+    {
+        if (i == s.length())
+            return true;
+
+        String prefix = "";
+
+        for (int j = i; j < s.length(); j++) {
+            prefix += s.charAt(j);
+
+            // Check if the prefix exists in the dictionary
+            if (Arrays.asList(dictionary).contains(prefix)
+                && wordBreakRec(j + 1, s, dictionary)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean wordBreak(String s, String[] dictionary)
+    {
+        return wordBreakRec(0, s, dictionary);
+    }
